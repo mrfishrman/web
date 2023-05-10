1b0b4acec7ac6267d8f3d8024bf8f25965d62f3e
mrfishrman <sebastian.fuentes.a@mail.pucv.cl>
2023-05-10 01:16:32 -0400

    first web

---
+++ cgi-bin/first.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import cgitb
+
+cgitb.enable()
+
+print("Content-Type: text/html:charset+utf-8")
+print("Content-type:text/html\r\n")
+print("<H1> Hello, From python server :) </H1>")
---
+++ cgi-bin/hello_post.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import cgi
+import cgitb
+import os
+
+cgitb.enable()
+
+#create instance of FieldStorage
+form = cgi.FieldStorage()
+
+#get data from fields
+first_name = form.getvalue('first_name')
+last_name = form.getvalue('last_name')
+
+print("Content-Type: text/html;charset=utf-8")
+print("Content-type:text/html\r\n")
+print("<!DOCTYPE html>")
+print("<html lang='en'>")
+print("<head>")
+print("<title>Hello - get CGI Progam</title>")
+print("</head>")
+print("<body>")
+print("<h2>Hello %s %s</h2>" % (first_name, last_name))
+print("</body>")
+print("</html>")
